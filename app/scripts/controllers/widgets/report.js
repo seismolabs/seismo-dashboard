b1fb24753dd976eb686bf96e1f061373c589c236
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-01-08 13:37:14 +0200

    refactored to directives

---
+++ app/scripts/controllers/widgets/report.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('dashboardApp').directive('report', function (analytics) {
+	var dates = {
+		today: moment(),
+		yesterday: moment().subtract('days', 1),
+		lastWeek: moment().startOf('week').subtract('weeks', 1),
+		lastMonth: moment().startOf('month').subtract('month', 1)
+	};
+
+	return {
+		restrict: 'E',
+		templateUrl: 'templates/report.html',
+		scope: true,
+		link: function ($scope, elem, attrs) {
+			$scope.heading = attrs.periodHeading;
+			$scope.datePeriod = dates[attrs.date].format('DD/MM/YYYY');
+
+			analytics.report(attrs.report, dates[attrs.date], 'user-registered', function (data) {
+				$scope.registered = data;
+			});
+
+			analytics.report(attrs.report, dates[attrs.date], 'user-logged-on', function (data) {
+				$scope.loggedOn = data;
+			});
+
+			analytics.report(attrs.report, dates[attrs.date], 'network-created', function (data) {
+				$scope.networksCreated = data;
+			});
+
+			analytics.report(attrs.report, dates[attrs.date], 'search', function (data) {
+				$scope.searches = data;
+			});
+
+			analytics.report(attrs.report, dates[attrs.date], 'share-with-friend', function (data) {
+				$scope.sends = data;
+			});
+
+			analytics.report(attrs.report, dates[attrs.date], 'share-like', function (data) {
+				$scope.shares = data;
+			});
+		}
+	};
+});
